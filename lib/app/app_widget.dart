0d2b621451478f7547d4cc4e7445cbb7722637fb
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-06-19 15:26:51 -0300

    first commit

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:controla_expedicao/app/features/home_page/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
